6436305eb97d7bf8b1c881f8c4d0bab970b2dcaf
Gust <gergus01@roberthalf.com>
2017-03-14 16:49:30 -0700

    put class into an IIFE

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-const gg = new Naljs()
+// const gg = new Naljs()
 
-console.log(gg.createDialog('hello nal!!'))
+// let elem = document.getElementById('createDialog-btn')
+
+// elem.addEventListener('click', function (e) {
+//   elem.parentNode.append(gg.createDialog(changeVid({chid: 'f0506eb8-ea24-4c99-a217-f0ee4f1b911f', vid: '52c42da6-22ff-4fa1-93c1-356fc61722f7'})))
+// })
+function createDialog (content) {
+  var elem = document.createElement('div')
+  elem.className = 'dialog-box'
+  var d = '<div class="dialog-box-content"><a href="#" class="dialog-close" onclick="this.parentNode.parentNode.remove()">&Cross;</a>' + content + '</div>'
+  elem.innerHTML = d
+
+  return elem
+}
+function changeVid (obj) {
+  if (obj === undefined || typeof obj !== 'object') {
+    obj = {
+      chid: '',
+      vid: ''
+    }
+  }
+
+  var iframeElem = "<iframe width=853 height=480 src='https://roberthalf.sharepoint.com/portals/hub/_layouts/15/VideoEmbedHost.aspx?" +
+              'chid=' + obj.chid +
+              '&vid=' + obj.vid +
+            "&amp;width=853&amp;height=480&amp;autoPlay=false&amp;showInfo=true' allowfullscreen data-spresponsive style='position: absolute; max-width: 100%; border: none;'></iframe>"
+
+  return iframeElem
+}
--- nal.js
+++ nal.js
@@ -1,24 +1,25 @@
-/* global naljs */
 /* nal.js - Not Another Library
  * ES6/2015 helper functions
  * © 2016 Gerard Gust
  */
-class Naljs {
-  createDialog (content) {
-    const d = `
-      <div class='dialog-box'>
-        <div class="dialog-box-content">
-          <a href="#" class="dialog-close" onclick="this.parentNode.parentNode.remove()">&Cross;</a>
-          ${content}
-        </div>
-      </div>
-    `
 
-    return d.replace(/\s{2,}/g, '') // .replace(/\'/g, '"') not sure if needed for minify
+/* ES6 code */
+const naljs = (function () {
+  class Naljs {
+    createDialog (content) {
+      let elem = document.createElement('div')
+      elem.className = 'dialog-box'
+      const d = `
+          <div class="dialog-box-content">
+            <a href="#" class="dialog-close" onclick="this.parentNode.parentNode.remove()">&Cross;</a>
+            ${content}
+          </div>
+      `
+      elem.innerHTML = d
+
+      return elem
+    }
   }
-}
-
-// Dev - Comment Out or Delete for Production or if not developing
-// fix for eslint standard no unused variables
-var nalDev = new Naljs()
-console.log(nalDev)
+  return new Naljs()
+}())
+console.log(naljs.constructor.name) // fix for eslint standard no unused variables
